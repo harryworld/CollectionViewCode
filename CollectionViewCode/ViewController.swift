0a4a41c1cc767e0e23ff1dffc5ee5599dda968d5
Harry Ng <harryworld@gmail.com>
2016-03-31 23:23:12 +0800

    Initial Commit

---
+++ CollectionViewCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CollectionViewCode
+//
+//  Created by Harry Ng on 31/3/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
